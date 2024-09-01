8012c0f059559ed4ba8d955999c44eb2c3bb473a
CrackLegends259 <crack.legends.fnf.259@gmail.com>
2024-09-01 12:55:23 -0500

    Add files via upload

---
+++ assets/preload/data/Love-Driven/hidehud.lua
@@ -0,0 +1,5 @@
+function onCreate()
+
+doTweenAlpha('GUItween', 'camHUD', 0, 0.000001, 'linear');
+
+end
---
+++ assets/preload/data/Love-Driven/opponentzoom.lua
@@ -0,0 +1,12 @@
+function onUpdate()
+   if mustHitSection then
+
+      setProperty('defaultCamZoom', 0.85)
+
+   else
+
+      setProperty('defaultCamZoom', 1)
+
+   end
+
+end
